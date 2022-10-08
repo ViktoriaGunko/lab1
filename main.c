118e59899feecb05d3dc0067a5af6acfe9282ac4
ViktoriaGunko <vika.gunko05@gmail.com>
2022-10-08 21:43:15 +0300

    Add files

---
+++ main.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define Name "Vika"
+#define LastName "Gunko"
+#define GroupName "RI-21"
+#define Faculty "RTF"
+#define University "Igor Sikorsky Kyiv Polytechnic Institute"
+
+
+int main()
+{   unsigned int semestr;
+    float admissionScore;
+    int grade1;
+    int grade2;
+    int grade3;
+    double averageGrade;
+    unsigned int age;
+
+
+    printf("%s %s follow next steps.\n", Name, LastName);
+
+    printf("Enter semestr number: ");
+    scanf("%u", &semestr);
+
+    printf("Enter your admission score:");
+    scanf("%f", &admissionScore);
+
+    printf("Enter a grade for subject ¹1:");
+    scanf("%d", &grade1);
+
+    printf("Enter a grade for subject ¹2:");
+    scanf("%d", &grade2);
+
+    printf("Enter a grade for subject ¹3:");
+    scanf("%d", &grade3);
+
+    printf("Enter your age:");
+    scanf("%u", &age);
+
+    averageGrade=(double)(grade1 + grade2 + grade3)/3;
+
+    system("cls");
+
+    printf("%s", University);
+
+    printf("\n%s", Faculty);
+
+    printf("\n%s", GroupName);
+
+    printf("\n---------------------------");
+
+    printf("\nstudent: %s %s", Name, LastName );
+
+    printf("\nage: %u", age);
+
+    printf("\nsemestr: %u", semestr);
+
+    printf("\n---------------------------");
+
+    printf("\nadmission score: %.2f", admissionScore);
+
+    printf("\naverage grade = %.3lf", averageGrade);
+
+    printf("\n");
+
+    return 0;
+}
